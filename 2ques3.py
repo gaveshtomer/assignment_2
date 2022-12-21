5633c92307b003a799f35c6c88adcdb9a0811e71
gaveshtomer <121171989+gaveshtomer@users.noreply.github.com>
2022-12-21 19:00:50 +0530

    Add files via upload

---
+++ 2ques3.py
@@ -0,0 +1,20 @@
+a = 56
+# a (binary) = 111000
+b = 10
+# b (binary) =   1010
+# QUESTION 1 --- Part(a):
+print(a & b)
+
+# QUESTION 1 --- Part(b):
+print(a | b)
+
+# QUESTION 1 --- Part(c):
+print(a ^ b)
+
+# QUESTION 1 --- Part(d):
+print(a << 2)
+print(b << 2)
+
+# QUESTION 1 --- Part(e):
+print(a >> 2)
+print(b >> 4)
