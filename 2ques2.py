2310919b7cad5c1568c8cceecd0994027bca4cb9
gaveshtomer <121171989+gaveshtomer@users.noreply.github.com>
2022-12-21 18:53:17 +0530

    Add files via upload

---
+++ 2ques2.py
@@ -0,0 +1,6 @@
+name = "gavesh"
+SID = 22104117
+department_name = "Electrical Engineering"
+CGPA= 9.9
+print(f"Hey,{name} Here!\nMy SID is {SID} \nI am from {department_name} department and my CGPA is {CGPA}")
+    
